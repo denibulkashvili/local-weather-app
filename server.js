1b43d13488f1b709eda44e2b184c8a898565345e
sharad jain <sharadJay@users.noreply.github.com>
2018-12-10 16:54:39 +0530

    Updating port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/scripts', express.static(`${__dirname}/src/scripts`));
 app.use('/styles', express.static(`${__dirname}/src/styles`));
